982726a5b5f2f39087ce1649cbbf3326c9d584e3
leejongseok1 <twocvx1@gmail.com>
2023-12-22 21:29:52 +0900

    2023 / 12 / 22

--- codingtest/python/boj3009_네_번째_점.py
+++ codingtest/python/boj3009_네_번째_점.py
@@ -1,7 +1,7 @@
 x = []
 y = []
 
-for _ in range(3):
+for i in range(3):
     a, b = map(int, input().split())
     x.append(a)
     y.append(b)
@@ -9,7 +9,7 @@ for _ in range(3):
 for i in range(3):
     if x.count(x[i]) == 1:
         x4 = x[i]
-    if y.count(x[i]) == 1:
+    if y.count(y[i]) == 1:
         y4 = y[i]
 
 print(x4, y4)
