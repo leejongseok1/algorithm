7d5af9d14f8d1a22e876aee166af07c626beb216
leejongseok1 <twocvx1@gmail.com>
2023-12-30 21:51:07 +0900

    2023 / 12 / 30

---
+++ codingtest/python/boj2606_바이러스.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input()) # 노드 개수(컴퓨터 수)
+m = int(input()) # 연결선 수(간선 수)
+graph = [[] for i in range(n+1)]
+visited = [0] * (n+1)
+for _ in range(m):
+    
+    a, b = map(int, input().strip().split())
+    graph[a] += [b]
+    graph[b] += [a]
+
+def dfs(v):
+    visited[v] = 1
+    for i in graph[v]:
+        if visited[i] == 0:
+            dfs(i)
+
+dfs(1)
+print(sum(visited)-1) # 1번 컴퓨터를 제외하고 1번 컴퓨터와 연결된 개수 출력
+    
+# 1 2
+# [[], [2], [1], [], [], [], [], []]
+# 2 3
+# [[], [2], [1, 3], [2], [], [], [], []]
+# 1 5
+# [[], [2, 5], [1, 3], [2], [], [1], [], []]
+# 5 2
+# [[], [2, 5], [1, 3, 5], [2], [], [1, 2], [], []]
+# 5 6
+# [[], [2, 5], [1, 3, 5], [2], [], [1, 2, 6], [5], []]
+# 4 7
+# [[], [2, 5], [1, 3, 5], [2], [7], [1, 2, 6], [5], [4]]
