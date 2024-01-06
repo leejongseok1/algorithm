14af9c276dd198e8683d6452dd1f86b49e4a3f9a
leejongseok1 <twocvx1@gmail.com>
2024-01-06 23:00:22 +0900

    240106

---
+++ codingtest/python/programmers_이상한_문자_만들기.py
@@ -0,0 +1,17 @@
+def solution(s):
+    
+    answer = ''
+    words = s.split(' ')
+    
+    for word in words:
+        for i in range(len(word)):
+            if i % 2 == 0:
+                answer += word[i].upper()
+            else:
+                answer += word[i].lower()
+        answer += ' '
+    return answer.strip()
+
+s = "try hello world"
+
+print(solution(s))
