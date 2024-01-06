1175da7350feff41bd7fcd2296a0aaf21140b721
leejongseok1 <twocvx1@gmail.com>
2024-01-07 00:02:21 +0900

    240107

---
+++ codingtest/python/programmers_폰켓몬.py
@@ -0,0 +1,17 @@
+def solution(nums):
+    result = len(set(nums))
+    if result > len(nums)/2:
+        return len(nums)/2
+    return result
+
+# 너무 어렵게 생각한 듯
+def solution2(nums):
+    from itertools import combinations
+
+    monster = [list(set(i)) for i in set(list(combinations(nums, int(len(nums)/2))))] 
+    answer = [len(i) for i in monster]
+    
+    return max(answer)
+
+nums = [3,3,3,2,2,4]
+print(solution(nums))
