4a534ff3bab541a10f522e07f78f9c6376669340
leejongseok1 <twocvx1@gmail.com>
2024-01-08 23:17:00 +0900

    240108

--- codingtest/python/programmers_소수_찾기.py
+++ codingtest/python/programmers_소수_찾기.py
@@ -2,11 +2,15 @@ def solution(n):
     
     cnt = 0
     for i in range(2, n+1):
-        for j in range(2, i):
-            if i % j == 0:
-                break
-        else:
+        if is_prime(i):
             cnt += 1
     return cnt
+
+def is_prime(n):
+    
+    for i in range(2, int(n**0.5)+1):
+        if n % i == 0:
+            return False
+    return True
 
 print(solution(10))
