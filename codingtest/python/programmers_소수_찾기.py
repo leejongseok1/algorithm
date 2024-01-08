715cf686fd60e0ceb19f243b915bc1f5226d3e59
leejongseok1 <twocvx1@gmail.com>
2024-01-08 23:02:23 +0900

    240108

---
+++ codingtest/python/programmers_소수_찾기.py
@@ -0,0 +1,12 @@
+def solution(n):
+    
+    cnt = 0
+    for i in range(2, n+1):
+        for j in range(2, i):
+            if i % j == 0:
+                break
+        else:
+            cnt += 1
+    return cnt
+
+print(solution(10))
