9823eb3208227780a84b9643325a00ab9a081a30
leejongseok1 <twocvx1@gmail.com>
2024-01-22 21:59:55 +0900

    240122

--- codingtest/python/boj2751_수_정렬하기_2.py
+++ codingtest/python/boj2751_수_정렬하기_2.py
@@ -1,11 +1,16 @@
+# 메모리초과(sort, sorted)
+# 계수정렬 이용
+
 import sys
 input = sys.stdin.readline
 
 n = int(input())
+num = [0] * 10001
 
-num = []
 for _ in range(n):
-    num.append(int(input()))
+    num[int(input())] += 1
     
-for i in sorted(num):
-    print(i)
+for i in range(len(num)):
+    if num[i] != 0:
+        for _ in range(num[i]):
+            print(i)
