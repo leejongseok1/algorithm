1836c6341476cef9a1cee413b618924ff31e29e4
leejongseok1 <twocvx1@gmail.com>
2024-02-04 21:37:27 +0900

    240204

---
+++ codingtest/python/boj1934_최소공배수.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def gcd(a, b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a % b)
+
+def lcm(a, b):
+    return (a * b) // gcd(a, b)
+
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, input().split())
+    print(lcm(a, b))
