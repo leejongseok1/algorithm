1fe09028ed1ca6311a6f71d0fbac5927b7645fad
leejongseok1 <twocvx1@gmail.com>
2024-02-06 20:24:19 +0900

    240206

--- codingtest/python/boj1929_소수_구하기.py
+++ codingtest/python/boj1929_소수_구하기.py
@@ -22,3 +22,13 @@ m, n = map(int, input().split())
 primes = eratosthenes(m, n)
 for prime in primes:
     print(prime)
+
+# m, n = map(int, input().split())
+# for i in range(m, n+1):
+#     if i == 1:
+#         continue
+#     for j in range(2, int(i**0.5)+1):
+#         if i % j == 0:
+#             break
+#     else:
+#         print(i)
