41543b86375d79c170a8d497c456875b42f72b41
leejongseok1 <twocvx1@gmail.com>
2025-02-22 17:40:34 +0900

    2025.02.22

---
+++ codingtest_v2/other_/상하좌우.py
@@ -0,0 +1,25 @@
+import sys
+n = int(sys.stdin.readline())
+move = list(sys.stdin.readline().split())
+
+raw = 1
+col = 1
+for i in range(len(move)):
+    
+    next_raw = raw
+    next_col = col
+    
+    if move[i] == "R":
+        next_col += 1
+    elif move[i] == "L":
+        next_col -= 1
+    elif move[i] == "U":
+        next_raw -= 1
+    elif move[i] == "D":
+        next_raw += 1
+        
+    if 1 <= next_raw <= n and 1 <= next_col <= n:
+        raw = next_raw
+        col = next_col
+        
+print(raw, col)
