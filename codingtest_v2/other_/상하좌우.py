9693379d5984f1dcc9d7c294aa59ccc23ba8f16c
leejongseok1 <twocvx1@gmail.com>
2025-02-23 18:36:44 +0900

    다시 풀어보기 (2025.02.23)

--- codingtest_v2/other_/상하좌우.py
+++ codingtest_v2/other_/상하좌우.py
@@ -5,21 +5,22 @@ move = list(sys.stdin.readline().split())
 raw = 1
 col = 1
 for i in range(len(move)):
-    
     next_raw = raw
     next_col = col
     
-    if move[i] == "R":
+    if move[i] == 'R':
         next_col += 1
-    elif move[i] == "L":
+    elif move[i] == 'L':
         next_col -= 1
-    elif move[i] == "U":
+    elif move[i] == 'U':
         next_raw -= 1
-    elif move[i] == "D":
+    elif move[i] == 'D':
         next_raw += 1
-        
-    if 1 <= next_raw <= n and 1 <= next_col <= n:
+    
+    if 1 <= next_col <= n and 1 <= next_raw <= n:
         raw = next_raw
         col = next_col
         
-print(raw, col)
+print(col, raw)
+    
+    
